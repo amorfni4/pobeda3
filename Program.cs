80e2f631a585838127b4c586a5d4639cc60c9bd0
Shiza-1789 <77673753+Shiza-1789@users.noreply.github.com>
2021-01-27 12:38:06 +0300

    please

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class Umnojeniena2
+    {
+        private int umnojennoe;
+
+        private Umnojeniena2() { }
+        public Umnojeniena2(int vvod)
+        {
+            umnojennoe = vvod;
+        }
+
+        public void Umnojenie (int chislo)
+            {
+            umnojennoe = chislo * 2;
+            }
+
+        public int Vivod
+        {
+            get { return umnojennoe; }
+        }
+
+        public static void Main()
+        {
+            Umnojeniena2 ba = new Umnojeniena2(2);
+
+            Console.WriteLine(ba.Vivod);
+        }
+    }
+}
